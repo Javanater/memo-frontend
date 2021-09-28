7c0c0ab6380e2afe21aeea3adb620876736daaaf
mfortenberry <mfortenberry@yorksolutions.net>
2021-09-28 15:31:50 -0500

    add Button component and css styling

---
+++ src/components/Button.js
@@ -0,0 +1,5 @@
+const Button = (props) => {
+    return <button className='btn' style={{ backgroundColor: props.color }}>{props.text}</button>
+}
+
+export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+import Button from "./Button";
+
 const Header = () => {
     return (
         <header className='header'>
             <h1 className='header'>Memos</h1>
+            <Button text='Create' color='green'/>
         </header>
     )
 }
